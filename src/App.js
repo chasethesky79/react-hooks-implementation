b2b1664b3fa48ae5c16f049d36d21f03718083e0
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-11-26 18:21:06 -0600

    react-hooks-implementation
    Implemented multiple hooks application

--- src/App.js
+++ src/App.js
@@ -2,29 +2,46 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let values = [];
+let currentHook = 0;
+
 function App() {
 
+  currentHook = 0;
+
   const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+
   const handleChange = (event) => {
     setName(event.target.value);
   }
 
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  }
+
   return (
     <div class="name-input">
-        <h1>My name via a functional component is : {name}</h1>
+        <h1>My name via a functional component is : {name}, {lastName}</h1>
         <input type="text" value ={name} onChange = {handleChange}/>
+        <input type="text" value ={lastName} onChange = {handleLastNameChange}/>
       </div>
   );
 }
 
 function useState(initialValue) {
-  let value = initialValue;
 
+  if (!values[currentHook]) {
+     values[currentHook] = initialValue;
+  }
+
+  let hookIndex = currentHook;
   function setState(newValue) {
-    value = newValue;
+    values[hookIndex] = newValue;
     ReactDOM.render(<App/>, document.getElementById('root'))
   }
-  return [value, setState]
+  
+  return [values[currentHook++], setState]
 }
 
 export default App;
